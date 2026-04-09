8dd383d06adc3a20eb90a53538758099f93bd2b7
CRUZ CASTILLO JHOAN ANTONI <116601508+Jhoan2-cpu@users.noreply.github.com>
2026-04-09 11:10:37 -0500

    feat: agregar pagina publica para probar subida a cloudinary

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CloudinaryTestController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
@@ -7,6 +8,12 @@ use Laravel\Fortify\Features;
 Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
+
+Route::get('/cloudinary-test', [CloudinaryTestController::class, 'show'])
+    ->name('cloudinary.test');
+
+Route::post('/cloudinary-test', [CloudinaryTestController::class, 'store'])
+    ->name('cloudinary.test.store');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
